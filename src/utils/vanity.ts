f7077bf3f4096bb02a0860668938715951e37462
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-07-30 17:13:52 -0300

    reduzindo um pouco o tamanho da palavra procurada e adicionando arquivo com 3 respostas distintas com o hash que originou elas

--- src/utils/vanity.ts
+++ src/utils/vanity.ts
@@ -1,5 +1,5 @@
 export const verifyIfBeautyExists = (base58EncodedRandomNumber: string): boolean => {
-  const beauty = RegExp("L[Ee3][4aA][nN][dD][rR]o","g");
+  const beauty = RegExp("L[Ee3][4aA][nN][dD]","g");
   if(beauty.exec(base58EncodedRandomNumber)!==null){
     return true;
   }
